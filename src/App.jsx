7752d3a804e63b8b7451e09fb05c42a9f2b48219
nurtaj <nurtajkhanam777@gmail.com>
2024-09-22 23:12:37 +0600

    Calculator version 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css";
-
-import React from "react";
+import styles from "./App.module.css";
+import ButtonContainer from "./Components/ButtonContainer";
+import Display from "./Components/Display";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className={styles.calculator}>
+      <Display />
+      <ButtonContainer />
+    </div>
+  );
 };
 
 export default App;
